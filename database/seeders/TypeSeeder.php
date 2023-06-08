dca9d5fc5ad861dd42beebceac32195563f756ac
Paolacocc <coccettipaola@gmail.com>
2023-06-08 19:12:35 +0200

    Add seeder to technology

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -17,7 +17,7 @@ class TypeSeeder extends Seeder
     public function run()
     {
         $types = ['Front-End', 'Back-End', 'Full-Stack'];
-        b:foreach ($types as $type) 
+        foreach ($types as $type) 
         {
             $new_type = new Type();
             $new_type->name = $type;
