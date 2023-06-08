3461542ff9813ce7f745678ed9034d4efbd6f4eb
Paolacocc <coccettipaola@gmail.com>
2023-06-08 19:32:23 +0200

    Add technologis in show

--- resources/views/Admin/Projects/show.blade.php
+++ resources/views/Admin/Projects/show.blade.php
@@ -10,6 +10,15 @@
 <h4> Type: no type has been selected</h4>
 @endif
 
+<div class="mt-3">
+    <h5>Technologies: </h5>
+    @forelse ($project->technologies as $tech)
+        <span>{{ $tech->name }} {{ $loop->last ? '' : ',' }}</span>
+    @empty
+        <span>No technology selected</span>
+    @endforelse
+
+</div>
 
 <p class="mt-4">{{ $project->description }}</p
     
